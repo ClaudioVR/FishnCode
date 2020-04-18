88bfe74c130a486c4063f365a445fa81c7960137
Claudio Rescigno <cludso@yahoo.it>
2020-04-18 11:24:43 +0200

    Fish n Code nuxt_code uploaded

---
+++ FishnCode_nuxt_code/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
